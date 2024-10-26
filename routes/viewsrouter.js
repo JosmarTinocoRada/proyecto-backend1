f9fffaf555e113e7262d5d291bb661725fae1a7e
josmar <josmariso28@gmail.com>
2024-10-25 23:49:03 -0300

    re entrega entrega final commit 1

--- routes/viewsrouter.js
+++ routes/viewsrouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const Product = require('../views/modelos/cart'); // Asumiendo modelo de productos
+const Cart = require('../views/modelos/products'); // Asumiendo modelo de carrito
 
 let products = require('./products').products;
 
@@ -20,6 +21,15 @@ router.get('/products', (req, res) => {
     res.render('realTimeProducts', { products, title: 'Productos en Tiempo Real' });
 });
 
+router.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('products', { products });
+});
+
+router.get('/cart', async (req, res) => {
+    const cart = await Cart.findOne({}).populate('items.product');
+    res.render('cart', { cart });
+});
 
 const emitProductEvents = (event, product) => {
     if (io) {
